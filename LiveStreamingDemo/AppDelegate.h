f8e1d891868bcaad712e1b5e46104adb83205286
reborn <343633674@qq.com>
2016-11-21 15:07:47 +0800

    Initial Commit

---
+++ LiveStreamingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveStreamingDemo
+//
+//  Created by 马英 on 16/11/21.
+//  Copyright © 2016年 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveStreamingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStreamingDemo
+//
+//  Created by 马英 on 16/11/21.
+//  Copyright © 2016年 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
